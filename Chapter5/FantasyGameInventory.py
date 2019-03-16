d82a5ba7e5476df989fd08b96ad081279c5771ec
Toan-PC\Toan <toanlathanh@gitbucket.org>
2019-03-15 22:15:03 -0500

    add chapter 5 solution

---
+++ Chapter5/FantasyGameInventory.py
@@ -0,0 +1,22 @@
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(v)+' '+k)
+        item_total+=v  # addup the number of items
+    print("Total number of items: " + str(item_total))
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory.setdefault(item,0)
+        inventory[item]+=1
+    return inventory
+    
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(stuff)
+displayInventory(inv)
